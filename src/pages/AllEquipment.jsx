baa8b8549c90e9acee2aeb0f0835ad0ba91120bb
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2025-01-25 11:01:55 +0530

    changes

--- src/pages/AllEquipment.jsx
+++ src/pages/AllEquipment.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Navbar from '../components/Navbar'
-import './allequipment.css'
+ import './allequipment.css'
 import Footer from "../components/Footer";
 const AllEquipment = () => {
   // Sample product data
